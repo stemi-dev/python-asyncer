8782e622b1291e13e07be30fa9ee9874a5a04df0
Fran Zekan <zekan.fran369@gmail.com>
2021-11-08 03:07:53 +0100

    New stuff + autodeploy

---
+++ publish.js
@@ -0,0 +1,34 @@
+const zx = require("zx");
+const fs = require("fs/promises");
+const { parse, inc } = require("semver");
+
+const pkg = require("./package.json");
+
+(async () => {
+  if (process.argv.length !== 3) {
+    console.log("You need exactly one param (major/minor/patch)");
+    process.exit(1);
+  }
+
+  const action = process.argv[2];
+  if (!["major", "minor", "patch"].includes(action)) {
+    console.log("Action not supported");
+    process.exit(1);
+  }
+
+  // @ts-ignore
+  const newVersion = inc(pkg["version"], action);
+  console.log(newVersion);
+
+  // @ts-ignore
+  pkg.version = newVersion;
+
+  await fs.writeFile("./package.json", JSON.stringify(pkg, null, 2));
+
+  const message = `"New release ${newVersion}"`;
+
+  zx.$`git add package.json`;
+  zx.$`git commit -m ${message}`;
+  zx.$`git push`;
+  zx.$`gh release create v${newVersion} --notes ${message}`;
+})();
