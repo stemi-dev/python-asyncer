b10f12532fb1907b7c229a9a8c8f99d96e035365
Fran Zekan <zekan.fran369@gmail.com>
2021-11-14 15:21:34 +0100

    New build stuff

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,12 @@
 const { join } = require("path");
 const esbuild = require("esbuild");
-const rimraf = require("rimraf");
 
 (async () => {
-  rimraf.sync(join(__dirname, "../dist"));
-
   await esbuild.build({
     entryPoints: [join(__dirname, "../lib/index.ts")],
     bundle: true,
     platform: "node",
     target: ["node14"],
-    outfile: join(__dirname, "../dist/index.js"),
+    outfile: join(__dirname, "../index.js"),
   });
 })();
--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -5,9 +5,7 @@ const { promisify } = require("util");
 const rimraf = promisify(require("rimraf"));
 
 (async () => {
-  await rimraf(join(__dirname, "../browser"));
-  await rimraf(join(__dirname, "../node"));
-
-  await rename(join(__dirname, "../dist/node"), join(__dirname, "../node"));
-  await rename(join(__dirname, "../dist/browser"), join(__dirname, "../browser"));
+  await rimraf(join(__dirname, "../*.d.ts"));
+  await rimraf(join(__dirname, "../templates"));
+  await rimraf(join(__dirname, "../index.js"));
 })();
