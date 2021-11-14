e8a1480cda255f6ced1e0984e8a5ce001db51d11
Fran Zekan <zekan.fran369@gmail.com>
2021-11-14 15:09:27 +0100

    "New release 2.0.0"

--- scripts/build.js
+++ scripts/build.js
@@ -1,35 +1,15 @@
 const { join } = require("path");
 const esbuild = require("esbuild");
 const rimraf = require("rimraf");
-// @ts-ignore
-const browserslistToEsbuild = require("browserslist-to-esbuild");
-
-/**
- * @type {import('esbuild').BuildOptions}
- */
-const shared = {
-  entryPoints: [join(__dirname, "../lib/index.ts")],
-  bundle: true,
-};
 
 (async () => {
   rimraf.sync(join(__dirname, "../dist"));
 
-  await Promise.all([
-    esbuild.build({
-      ...shared,
-      minify: true,
-      sourcemap: true,
-      platform: "browser",
-      target: "es2020",
-      outfile: join(__dirname, "../dist/browser/index.js"),
-    }),
-
-    esbuild.build({
-      ...shared,
-      platform: "node",
-      target: ["node14"],
-      outfile: join(__dirname, "../dist/node/index.js"),
-    }),
-  ]);
+  await esbuild.build({
+    entryPoints: [join(__dirname, "../lib/index.ts")],
+    bundle: true,
+    platform: "node",
+    target: ["node14"],
+    outfile: join(__dirname, "../dist/index.js"),
+  });
 })();
