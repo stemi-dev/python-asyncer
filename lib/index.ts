a8fb1f7486fb9cf767ed01a22836f830aa8036ee
Fran Zekan <zekan.fran369@gmail.com>
2022-01-12 14:56:42 +0100

    "New release 3.1.6"

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 export { asyncify, AsyncifyENV } from "./asyncify";
 export { generateTest, GeneratedTest, TemplateTests } from "./generateTest";
 
+export { cleanup } from "./utils";
+
 export * from "./const";
