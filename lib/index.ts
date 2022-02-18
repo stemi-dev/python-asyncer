f034617c08791532bf6a76a00d952214b21d3015
Fran Zekan <zekan.fran369@gmail.com>
2022-02-18 18:21:44 +0100

    "New release 4.0.2"

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,5 @@ export { generateTest, GeneratedTest, TemplateTests } from "./generateTest";
 export { cleanup } from "./utils";
 
 export { tracebackFormatter } from "./helpers/traceback";
-import * as tracebackHelpers from "./helpers/traceback";
-export { tracebackHelpers };
 
 export * from "./const";
