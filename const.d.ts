4a38012b8c47afdca7129c7b0a896d9a5928842d
Luka Dumančić <luka.dumancic49@gmail.com>
2023-10-26 15:12:23 +0200

    new version

---
+++ const.d.ts
@@ -0,0 +1,7 @@
+export declare const DEFAULT_INDENTS = 4;
+export declare const STDIO_NAMES: {
+    print: string;
+    input: string;
+};
+export declare const INTERNAL_FUNC_NAME_USER_CODE = "internal_func_name_user_code";
+export declare const MAIN_FUNCTION = "main";
