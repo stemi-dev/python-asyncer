caea517c3bd32e4426bb19f63a818c1118140f0f
Luka Dumančić <luka.dumancic49@gmail.com>
2023-11-23 14:02:44 +0100

    New package version

---
+++ utils.d.ts
@@ -0,0 +1,4 @@
+import { GeneratedTest } from "./generateTest";
+export declare const space: (n: number) => string;
+export declare const cleanup: (code: string) => string[];
+export declare const formatTestData: (data: GeneratedTest) => string;
