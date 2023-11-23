7314afd67c2227594e78d4560a20ebf28fb57ec8
Luka Dumančić <luka.dumancic49@gmail.com>
2023-11-23 14:03:42 +0100

    "New release 4.9.0"

--- templates/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { polyfills } from "./polyfills";
-export { run } from "./run";
-export declare const shared = "import json\nimport re\nimport traceback";
--- templates/polyfills.d.ts
+++
@@ -1,5 +0,0 @@
-import { AsyncifyENV } from "../asyncify";
-export declare const polyfills: Record<AsyncifyENV, ({ input, print }: {
-    input: any;
-    print: any;
-}) => string>;
