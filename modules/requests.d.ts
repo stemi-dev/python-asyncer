bab01f9412bf2167e13429ca45d5a629d53e1d5e
Luka Dumančić <luka.dumancic49@gmail.com>
2024-01-10 14:44:04 +0100

    "New release 5.2.0"

--- modules/requests.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\nfrom pyodide.http import pyfetch\n\nasync def get(url):\n    response = await pyfetch(url)\n    return response\n\n";
-export default _default;
--- utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { GeneratedTest } from "./generateTest";
-export declare const space: (n: number) => string;
-export declare const addCustomModules: (code: string) => string;
-export declare const cleanup: (code: string) => string[];
-export declare const formatTestData: (data: GeneratedTest) => string;
