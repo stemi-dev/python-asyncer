6e5e08fe20cd3cef6359ed75af69cbdfab658596
Filip Beleta <filip.beleta@gmail.com>
2024-07-24 12:35:27 +0200

    Fix string funtions

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ var cleanup = (code) => {
   code = code.split("\n").map((line, index) => `${line} #|LINE_NUM:${index + 1}|#`).join("\n");
   code = code.replace(/\\n/g, "\\\\n");
   const tmp = code.split("f'''").join("#JOIN#").split('f"""').join("#JOIN#").split('"""').join("#JOIN#").split("'''").join("#JOIN#").split("#JOIN#");
+  console.log(tmp)
   let out = "";
   for (let i = 0; i < tmp.length; i++) {
     if (i % 2 === 0) {
@@ -106,6 +107,18 @@ var cleanup = (code) => {
   }
   let lines = out.split("\n").filter((a) => !a.startsWith("#"));
   lines = lines.map((el) => el.includes("\\\\n") ? el.replaceAll("\\\\n", "\\n") : el);
+  let openMultiline = false;
+
+  lines = lines.map((el) => {
+    if (openMultiline) {
+      el = el.trim();
+    }
+    if (el.includes("'''") || el.includes('""""')){
+      openMultiline = !openMultiline
+    }
+    return el;
+  })
+
   if (funcs)
     funcs.forEach((fnel) => {
       if (fnel) {
